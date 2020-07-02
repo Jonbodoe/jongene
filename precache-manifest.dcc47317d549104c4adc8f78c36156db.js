4ab75637c12644ece4145d60b62f4a1a0878c27f
Jonbodoe <le0220@mail.philau.edu>
2020-07-02 18:35:22 -0400

    Updates

---
+++ precache-manifest.dcc47317d549104c4adc8f78c36156db.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca780497d8c0aba40ae8a801a3b43bb",
+    "url": "/jongene/index.html"
+  },
+  {
+    "revision": "6dfd9622ce51fb871486",
+    "url": "/jongene/static/css/main.4871be42.chunk.css"
+  },
+  {
+    "revision": "a45b353ed04e85452e11",
+    "url": "/jongene/static/js/2.79553752.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jongene/static/js/2.79553752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dfd9622ce51fb871486",
+    "url": "/jongene/static/js/main.5f1bef0a.chunk.js"
+  },
+  {
+    "revision": "d8132e4d19c9ac7381e9",
+    "url": "/jongene/static/js/runtime-main.73b955c6.js"
+  },
+  {
+    "revision": "e354a1d55690c439fd28f8f388f7e43e",
+    "url": "/jongene/static/media/bboy.e354a1d5.jpg"
+  },
+  {
+    "revision": "839bacd5427165131f4388045e422ea1",
+    "url": "/jongene/static/media/car.839bacd5.jpg"
+  },
+  {
+    "revision": "918651d8f6dc895abce838aad69f49d3",
+    "url": "/jongene/static/media/chinatown.918651d8.jpg"
+  },
+  {
+    "revision": "76d6333b051ebc1e661000c5aa643c88",
+    "url": "/jongene/static/media/city-hall.76d6333b.jpg"
+  },
+  {
+    "revision": "934e20f75567cd2a53d9a7040acc91d6",
+    "url": "/jongene/static/media/coffee.934e20f7.jpg"
+  },
+  {
+    "revision": "368d1f2217b9f144548bf56e37e997cd",
+    "url": "/jongene/static/media/csx.368d1f22.jpg"
+  },
+  {
+    "revision": "92370f5928f98a15954ff288fe908c5a",
+    "url": "/jongene/static/media/doit.92370f59.jpg"
+  },
+  {
+    "revision": "b7bb7393cf6ade589efffdebba977622",
+    "url": "/jongene/static/media/fish-tank.b7bb7393.jpg"
+  },
+  {
+    "revision": "b5af7f94ca2d39e03a7af66fe15c1928",
+    "url": "/jongene/static/media/group.b5af7f94.jpg"
+  },
+  {
+    "revision": "677b4e3e96880ff2204822803285db49",
+    "url": "/jongene/static/media/k-and-a.677b4e3e.jpg"
+  },
+  {
+    "revision": "3a0d073162d9b379aff19fc812fb5b8f",
+    "url": "/jongene/static/media/navy-yard.3a0d0731.jpg"
+  },
+  {
+    "revision": "31695b0ca729beaf54f52f897c6db6f3",
+    "url": "/jongene/static/media/nike-shoes.31695b0c.jpg"
+  },
+  {
+    "revision": "17262ed94b16efbdc7452b1966706ae7",
+    "url": "/jongene/static/media/pat-pizza.17262ed9.jpg"
+  },
+  {
+    "revision": "36f603dc8c3b19329ad9f3baf98cb3a7",
+    "url": "/jongene/static/media/pink-bg.36f603dc.jpg"
+  },
+  {
+    "revision": "f8e58a0b407f2e89c5dc2db14bdb29cf",
+    "url": "/jongene/static/media/pink-outfit.f8e58a0b.jpg"
+  },
+  {
+    "revision": "23275e565533bf081e2d5980da04125a",
+    "url": "/jongene/static/media/skyscraper.23275e56.jpg"
+  },
+  {
+    "revision": "a2dba714d7b6d019024f2f7f91d2a5a5",
+    "url": "/jongene/static/media/smalls.a2dba714.jpg"
+  },
+  {
+    "revision": "1a7c47b73cb4cf17491f9ab3480d4de5",
+    "url": "/jongene/static/media/stairs.1a7c47b7.jpg"
+  },
+  {
+    "revision": "b83e2ee971e2cdeb099b4f3a58d08003",
+    "url": "/jongene/static/media/tokyo.b83e2ee9.jpg"
+  },
+  {
+    "revision": "6895d50bc09360f65e99318c6df3f8bb",
+    "url": "/jongene/static/media/tokyo2.6895d50b.jpg"
+  },
+  {
+    "revision": "2f6b3217f8b268dcffa8cb18f29c0509",
+    "url": "/jongene/static/media/tokyo4.2f6b3217.jpg"
+  },
+  {
+    "revision": "4473d8515c04642f34a1afcf2113ed38",
+    "url": "/jongene/static/media/tokyo5.4473d851.jpg"
+  },
+  {
+    "revision": "92b3bc6328530d2cb32c6b4217dde10e",
+    "url": "/jongene/static/media/viaduct.92b3bc63.jpg"
+  },
+  {
+    "revision": "de91a205c3b063783d6d6059f3c662fe",
+    "url": "/jongene/static/media/yellow.de91a205.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42775739c58d142aac21e94c211834e3.js"
+  "/jongene/precache-manifest.dcc47317d549104c4adc8f78c36156db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jongene/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
