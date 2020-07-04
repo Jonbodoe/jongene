bb91daab43f954d3bdc953c73cc6c1ccee570b32
Jonbodoe <le0220@mail.philau.edu>
2020-07-04 00:15:23 -0400

    edited media queries

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,20 +10,14 @@ const About = () => {
                 <Fade bottom>
                     <div className="col-lg-2"></div>
                     <div id="profile-img" className="col-lg-6">
-
-                        {/* <img className="profile-img" src={require('./../img/protraitshot.jpeg')}/> */}
                     </div>
                     <div className="col-lg-4 p-3 d-flex align-self-center p-lg-4">
-                        {/* <div className=""> */}
-
                         <div className="p-4">
                             <h2 className="py-2">About</h2>
                             <div className="border-bottom border-secondary pt-2"></div>
                             <p className="py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                         </div>
-
                     </div>
-                    {/* </div> */}
                 </Fade>
             </div>
 
